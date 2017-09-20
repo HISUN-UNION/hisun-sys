6590e96d42bd4330783c3aecb9566f22f4e635b6
rockwithyou <rockwithyou@126.com>
2017-09-20 16:23:02 +0800

    Modify  Sha01gbrmspbController.java etc.

--- src/main/java/com/hisun/saas/zzb/app/console/shpc/controller/Sha01gbrmspbController.java
+++ src/main/java/com/hisun/saas/zzb/app/console/shpc/controller/Sha01gbrmspbController.java
@@ -85,12 +85,12 @@ public class Sha01gbrmspbController extends BaseController {
                 }
             }else{
                 map.put("code", -1);
-                map.put("message", "请上传word");
+                map.put("message", "请上传word!");
                 return map;
             }
         }catch(Exception e){
             map.put("code", -1);
-            map.put("message", "读取文件错误，请检查word是否能正确打开");
+            map.put("message", "读取文件错误!");
             return map;
         }
         try{
@@ -109,6 +109,94 @@ public class Sha01gbrmspbController extends BaseController {
         map.put("code", 1);
         return map;
     }
+
+
+
+    @RequestMapping(value="/batch/upload")
+    public @ResponseBody
+    Map<String,Object> batchUpload(String pcId, @RequestParam(value="gbrmspbFile",required=false) MultipartFile file, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        UserLoginDetails userLoginDetails = UserLoginDetailsUtil.getUserLoginDetails();
+        Map<String,Object> map = new HashMap<String,Object>();
+        if(file==null || file.isEmpty()){
+            map.put("code", -1);
+            map.put("message", "文件没有内容");
+            return map;
+        }
+        //模板路径
+        String wordTemplatePath = uploadAbsolutePath+Sha01gbrmspbService.ATTS_PATH + "gbrmspb.docx";
+        try{
+            String fileName = file.getOriginalFilename();
+            if(fileName.toLowerCase().endsWith(".zip")){
+                File _fileDir = new File(uploadAbsolutePath+Sha01gbrmspbService.ATTS_PATH);
+                if (_fileDir.exists() == false) {
+                    _fileDir.mkdirs();
+                }
+                //原zip存储路径
+                String zipFile = uploadAbsolutePath+Sha01gbrmspbService.ATTS_PATH + UUIDUtil.getUUID()+".zip";
+                String tmpFilePath =  uploadAbsolutePath+Sha01gbrmspbService.ATTS_PATH+UUIDUtil.getUUID()+File.separator;
+                //解压到临时目录
+                CompressUtil.unzip(zipFile,tmpFilePath);
+                //循环目录下的文件,如果在当前批次下找到对应名字的干部,则附加到当前干部下
+                File tempFiles = new File(tmpFilePath);
+                if(tempFiles!=null){
+                    for(File f : tempFiles.listFiles()){
+                        String fname = f.getName();
+                        List<Sha01> sha01s = this.sha01Service.list();
+                        if(sha01s!=null && sha01s.size()>0){
+                            String ext = f.getName().substring(f.getName().lastIndexOf("."));
+                            String savePath = _fileDir+UUIDUtil.getUUID()+ext;
+                            File desFile = new File(savePath);
+                            FileUtils.copyFile(f,desFile);
+                            Sha01gbrmspb sha01gbrmspb = new Sha01gbrmspb();
+                            sha01gbrmspb.setFilepath(savePath);
+                            sha01gbrmspb.setSha01(sha01s.get(0));
+                            this.sha01gbrmspbService.saveFromWord(sha01gbrmspb,savePath,wordTemplatePath);
+                        }
+                    }
+                }
+
+                //模板路径
+//                String wordTemplatePath = uploadAbsolutePath+Sha01gbrmspbService.ATTS_PATH + "gbrmspb.docx";
+//                FileOutputStream fos = new FileOutputStream(new File(savePath));
+//                fos.write(file.getBytes());
+//                fos.flush();
+//                fos.close();
+//
+//                Sha01 sha01 = this.sha01Service.getByPK(sha01Id);
+//                Sha01gbrmspb sha01gbrmspb = new Sha01gbrmspb();
+//                sha01gbrmspb.setFilepath(savePath);
+//                sha01gbrmspb.setSha01(sha01);
+//                this.sha01gbrmspbService.saveFromWord(sha01gbrmspb ,savePath,wordTemplatePath);
+            }else{
+                map.put("code", -1);
+                map.put("message", "请上传ZIP!");
+                return map;
+            }
+        }catch(Exception e){
+            map.put("code", -1);
+            map.put("message", "读取文件错误!");
+            return map;
+        }
+        try{
+
+        }catch(GenericException e){
+            logger.error(e, e);
+            map.put("code", -1);
+            map.put("message", e.getMessage());
+            return map;
+        }catch(Exception e){
+            logger.error(e, e);
+            map.put("code", -1);
+            map.put("message", "系统错误，请联系管理员");
+            return map;
+        }
+        map.put("code", 1);
+        return map;
+    }
+
+
+
+
     @RequestMapping(value="/ajax/down")
     public void templateDown(String sha01Id,HttpServletRequest req, HttpServletResponse resp) throws Exception{
         Sha01 sha01 = this.sha01Service.getByPK(sha01Id);
